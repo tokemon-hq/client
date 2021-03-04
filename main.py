61631fdf741b457bc2d9377c0898aa2953e76d9e
tokemon-hq <tokemon-hq@tokemon.io>
2021-03-04 18:36:52 +0100

    Add a GUI
    This is a simple Tk gui that shows the current config values. It also
    shows connection state and does some config sanity checking. It needs
    only a reconnnect button and some thread-state management for the 1st
    launch user-case
    
    It defaults to enabled on Windows but not on linux or when Tk is missing
    
    Written in Tk only for no extra dependencies and a simpler single-file
    executable experience.
    
    A bit of thread/async management mess here because of asyncio, but seems
    to work well.

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
+import os
+import hashlib
+import sys
+import inspect
+import logging
 import asyncio
 import json
+import time
+import platform
+import threading
+import itertools
+import collections
+
+try:
+    import tkinter as tk
+    from tkinter import ttk
+    from tkinter.messagebox import showerror
+    HAS_TK = True
+except ImportError:
+    # on linux without Tk installed
+    HAS_TK = False
+
 import websockets
+
 from uniswap import Uniswap
 from web3 import Web3
-import logging
 from dotenv import load_dotenv
-import os
-from time import sleep
+
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 gas_in_gwei = 0
+status = "Starting"
+
+
+def get_client_source():
+    return inspect.getsource(sys.modules[__name__]).encode('utf-8')
+
+
+def get_client_hash():
+    return hashlib.sha256(get_client_source()).hexdigest()
 
 
 def gas_price_strategy(web3, transaction_params):
@@ -18,6 +47,15 @@ def gas_price_strategy(web3, transaction_params):
 
 
 def to_dict(dict_to_parse):
+
+    def parse_value(_val):
+        if 'dict' in str(type(_val)).lower():
+            return to_dict(_val)
+        elif 'HexBytes' in str(type(_val)):
+            return _val.hex()
+        else:
+            return _val
+
     parsed_dict = dict(dict_to_parse)
     for key, val in parsed_dict.items():
         if 'list' in str(type(val)):
@@ -25,15 +63,6 @@ def to_dict(dict_to_parse):
         else:
             parsed_dict[key] = parse_value(val)
     return parsed_dict
-
-
-def parse_value(val):
-    if 'dict' in str(type(val)).lower():
-        return to_dict(val)
-    elif 'HexBytes' in str(type(val)):
-        return val.hex()
-    else:
-        return val
 
 
 async def uniswap_buy_input(input_token, output_token, input_quantity, max_slippage, max_gas, account, ethereum_provider):
@@ -62,7 +91,7 @@ def read_data_from_env():
     accounts = read_accounts_from_env()
     ethereum_provider = os.environ.get('ETHEREUM_PROVIDER')
     token = os.environ.get('TOKEN')
-    base_url = os.environ.get('BASE_URL')
+    base_url = os.environ.get('BASE_URL', 'txwsserver-prod.txmonitor.devops.mk')
     return {
         "username": username,
         "accounts": accounts,
@@ -72,9 +101,28 @@ def read_data_from_env():
     }
 
 
+class ConfigError(ValueError):
+    pass
+
+
+def check_config(env_data):
+    for n in ('username', 'token', 'base_url', 'ethereum_provider'):
+        if not env_data.get(n):
+            raise ConfigError("Missing '%s'" % n)
+
+    acts = env_data.get('accounts') or {}
+    if not acts:
+        raise ConfigError('No Configured Account')
+
+    for i, act in enumerate(acts.values()):
+        for n in ('name', 'address', 'pkey'):
+            if not act.get(n):
+                raise ConfigError("Account %d Missing '%s'" % (i + 1, n))
+
+
 def read_accounts_from_env():
-    num_accounts = int(os.environ.get("NUM_ACCOUNTS"))
-    accounts = {}
+    num_accounts = int(os.environ.get("NUM_ACCOUNTS", "0"))
+    accounts = collections.OrderedDict()
     for i in range(num_accounts):
         index = i + 1
         acc_data = {
@@ -90,10 +138,22 @@ def get_connection_uri(base_url: str, username: str):
     return f"wss://{base_url}/api/v1/users/subscribe/{username}"
 
 
-async def main():
-    env_data = read_data_from_env()
+def main_is_frozen():
+    return hasattr(sys, "frozen")
+
+
+def get_main_dir():
+    if main_is_frozen():
+        return os.path.dirname(sys.executable)
+    return os.path.dirname(sys.argv[0])
+
+
+async def main(env_data):
+    global status
 
     uri = get_connection_uri(env_data.get('base_url'), env_data.get('username'))
+    logger.info('Connecting to %s' % uri)
+
     accounts = env_data.get('accounts')
 
     connection_retry = True
@@ -102,13 +162,16 @@ async def main():
         async with websockets.connect(uri) as websocket:
 
             msg = {'code': 'login', 'token': env_data.get('token'), "accounts": list(accounts.keys())}
+            logger.debug('Authenticating: %r' % msg)
 
             await websocket.send(json.dumps(msg))
 
             msg_response = await websocket.recv()
+
             msg_response_dict = json.loads(msg_response)
             if msg_response_dict['code'] == 'auth_ok':
                 logger.info('Authentication successful.')
+                status = 'Connected'
                 fl_exit = False
                 try:
                     while not fl_exit:
@@ -155,8 +218,139 @@ async def main():
             else:
                 connection_retry = False
                 logger.error('Error while authenticating to the server. Please check your credentials in the .env file.')
+                status = 'Error Authenticating'
         if connection_retry:
-            sleep(10)
+            time.sleep(10)
 
 
-asyncio.get_event_loop().run_until_complete(main())
+class GUI(object):
+
+    def __init__(self, env_data):
+        self._root = tk.Tk()
+        self._root.title('Tokemon Trading Bot')
+
+        # self._root.geometry('500x100')
+        # self._root.resizable(width=True, height=True)
+
+        self._root.grid_columnconfigure(0, weight=1)
+        self._root.grid_rowconfigure(0, weight=1)
+
+        self._frame = ttk.Frame(self._root)
+        self._frame.columnconfigure(1, weight=1)
+        self._frame.columnconfigure(1, weight=3)
+
+        self._thread = None
+
+        def _label_entry(_lbl, _txt, _row, is_password=False, **gridpos):
+            l = ttk.Label(self._frame, text=_lbl)
+            l.grid(column=0, row=_row, sticky='W', padx=5, pady=5, **gridpos)
+
+            sv = tk.StringVar()
+            sv.set(_txt or '')
+            sve = ttk.Entry(self._frame, textvariable=sv, show="*" if is_password else None)
+            sve.grid(column=1, row=_row, padx=5, pady=5, sticky="EW", **gridpos)
+
+            return sv
+
+        self._gui_env_entries = {
+            'username': _label_entry('Tokemon Username', env_data.get('username') or '', 1),
+            'token': _label_entry('Tokemon Bot Token', env_data.get('token') or '', 2),
+            'ethereum_provider': _label_entry('Infura URL', env_data.get('ethereum_provider') or '', 3)
+        }
+
+        acts = env_data['accounts']
+        self._base_url = env_data['base_url']
+
+        def _acct_entry(_num, _act, _row0):
+            return {
+                'account_%d_name' % _num: _label_entry('Account %d Name' % _num, _act.get('name') or '', _row0),
+                'account_%d_address' % _num: _label_entry('Account %d Address' % _num, _act.get('address') or '', _row0 + 1),
+                'account_%d_pkey' % _num: _label_entry('Account %d Private Key' % _num, _act.get('pkey') or '', _row0 + 2, True)
+            }
+
+        row = 4
+        if len(acts) <= 1:
+            try:
+                act_1 = acts[tuple(acts.keys())[0]]
+            except IndexError:
+                act_1 = {'name': None, 'address': None, 'pkey': None}
+
+            self._gui_env_entries.update(_acct_entry(1, act_1, row))
+            row += 3
+
+        else:
+            for i, act_name in enumerate(acts):
+                act = acts[act_name]
+                self._gui_env_entries.update(_acct_entry(i + 1, act, row))
+                row += 3
+
+        self._result_label = ttk.Label(self._frame)
+        self._result_label.grid(row=row, columnspan=3, padx=5, pady=5)
+
+        self._frame.grid(padx=10, pady=10, sticky='NSEW')
+
+    def update_status(self):
+        msg = 'Status: %s' % status
+        self._result_label.configure(text=msg)
+        self._root.after(1000, self.update_status)
+
+    def get_env_data_from_gui(self):
+        d = {k: self._gui_env_entries[k].get() for k in ('username', 'token', 'ethereum_provider')}
+        d['base_url'] = self._base_url
+
+        accounts = {}
+        for i in itertools.count(1):
+            try:
+                act = {}
+                for j in ('name', 'address', 'pkey'):
+                    act[j] = self._gui_env_entries['account_%d_%s' % (i, j)].get()
+                accounts[act['name']] = act
+            except KeyError:
+                break
+
+        d['accounts'] = accounts
+        return d
+
+    def start_thread(self):
+        env_data = self.get_env_data_from_gui()
+
+        try:
+            check_config(env_data)
+        except ConfigError:
+            global status
+            status = 'Not Configured'
+            return
+
+        def run_thread():
+            asyncio.run(main(env_data))
+
+        self._thread = threading.Thread(target=run_thread, daemon=True)
+        self._thread.start()
+
+    def run(self):
+        self.start_thread()
+        self.update_status()
+        self._root.mainloop()
+
+
+def uimain(env_data):
+    gui = GUI(env_data)
+    gui.run()
+
+
+if __name__ == "__main__":
+
+    try:
+        _use_gui = sys.argv[1] == 'gui'
+    except IndexError:
+        # default to GUI on windows
+        _use_gui = platform.system() == 'Windows'
+
+    _env_data = read_data_from_env()
+    # print(_env_data)
+
+    if HAS_TK and (main_is_frozen() or _use_gui):
+        uimain(_env_data)
+    else:
+        check_config(_env_data)
+        asyncio.get_event_loop().run_until_complete(main(_env_data))
